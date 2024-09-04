5237cbb79056315acc3dfe8db04d3fa406d66ef9
KimMinGyeol <alsruf741@naver.com>
2024-09-04 08:41:35 +0000

    frontend deployment

--- src/main/Meet/api.js
+++ src/main/Meet/api.js
@@ -1,18 +1,14 @@
 import axios from 'axios';
 
-// Axios 인스턴스를 생성할 때 withCredentials 옵션을 설정
 const api = axios.create({
   baseURL: 'https://dev.tft.p-e.kr',
 });
-
-// 전역 설정 추가: 쿠키 기반 인증 정보를 요청 시 함께 전송
-axios.defaults.withCredentials = true;
 
 export const setAuthToken = token => {
   if (token) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
-    // 토큰이 없으면 권한 삭제
+    //토큰이 없으면 권한 삭제
     delete api.defaults.headers.common['Authorization'];
   }
 };
