3fbd3a2fc4303ac1d0013677c3dadf8694c235ef
KimMinGyeol <alsruf741@naver.com>
2024-09-04 07:48:18 +0000

    frontend deployment

--- src/Member/api.js
+++ src/Member/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://dev.tft.p-e.kr',
+  baseURL: 'https://dev.tft.p-e.kr',
 });
 
 export const setAuthToken = token => {
--- src/api.config.js
+++ src/api.config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";	
 }else {
-  backendHost = "http://tft-back01.ap-northeast-2.elasticbeanstalk.com";	//실제 배포할 주소
+  backendHost = "https://dev.tft.p-e.kr";	//실제 배포할 주소
 }
 
 export const API_BASE_URL = `${backendHost}`;
