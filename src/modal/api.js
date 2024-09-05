dca1310e012853c60604a1b3b4f7dfa1fad3cf20
Mingyeol <alsruf741@gmail.com>
2024-09-05 17:30:32 +0900

    연결오류해결7

--- src/modal/api.js
+++ src/modal/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 환경 변수에서 baseURL 값을 가져옴123
 const api = axios.create({
-  baseURL: 'https://dev.tft.p-e-kr'
+  baseURL: 'https://dev.tft.p-e.kr'
 });
 
 export const setAuthToken = token => {
