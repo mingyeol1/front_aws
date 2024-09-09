255b829e74cbfa812180b7963b62130ed8ad0b55
sungkyunpark1992 <117180248+sungkyunpark1992@users.noreply.github.com>
2024-09-09 21:53:05 +0900

    fix: 게시글 사용자 보안 기능 추가
    게시글 작성 및 수정 유저값 확인 후 진행; 프론트에서만 구현

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -2,15 +2,16 @@ import style from "../styles/LinkComponents.module.css";
 import { Link, NavLink, useParams } from "react-router-dom";
 
 const LinkButton = ({ className, color = "primary", size = "md", onClick, children }) => {
+    console.log(onClick);
+    console.log('LinkButton');
     return (
-        <button  
-            onClick={onClick} 
-            className={`${className} ${style[color]} ${style[size]} ${style.btn}`}
-        >
-            <Link to={`/boardcreate`}>
-            {children}
-            </Link>
-        </button>
+        <Link onClick={onClick} to={`/boardcreate`}>
+            <button
+                className={`${className} ${style[color]} ${style[size]} ${style.btn}`}
+            >
+                {children}
+            </button>
+        </Link>
     );
 };
 
